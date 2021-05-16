4222d7b71eb3bdc3ea73e44c2e8c0dda6277fa62
oobohP <stevenly1616@gmail.com>
2021-05-16 12:44:44 -0700

    changed app controller from ::api to base, for template rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-# /api/v1/headlines
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  # /api/v1/headlines
   namespace :api do
     namespace :v1 do
       resources :headlines
     end
   end
+
+  # unspecified routes will route to react
+  get '*path', to: 'pages#index', via: :all
 end
